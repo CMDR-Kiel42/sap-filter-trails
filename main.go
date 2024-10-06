1512aa0734c89455bf37a81b2e3232135cbfd58e
CMDR-Kiel42 <klideyla@gmail.com>
2024-10-06 16:09:09 -0400

    Couple more routes: with picnic and grills

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ type CSVBool struct {
 
 type Trail struct {
 	FID 			int		`csv:"FID"`
+	AccessName		string	`csv:"AccessName"`
 	HasRestrooms	CSVBool	`csv:"RESTROOMS"`
 	HasPicnic 		CSVBool	`csv:"PICNIC"`
 	HasFishing 		CSVBool	`csv:"FISHING"`
@@ -31,12 +32,13 @@ type Trail struct {
 	AccessID 		string	`csv:"AccessID"`
 	Class 			string	`csv:"Class"`
 	Address 		string	`csv:"Address"`
-	HasFee 			string	`csv:"Fee"`
+	HasFee 			CSVBool	`csv:"Fee"`
 	IsBikeTrail		CSVBool	`csv:"BikeTrail"`
+	HasGrills		CSVBool	`csv:"Grills"`
 }
 
 var trails = []Trail{}
-
+var bikeTrails = []Trail{}
 
 func (csvBool *CSVBool) UnmarshalCSV(csvValue string) (err error) {
 	if csvValue == "Yes" {
@@ -56,7 +58,7 @@ func (csvBool *CSVBool) MarshalJSON() ([]byte, error) {
 	return json.Marshal(parsedValue)
 }
 
-func parseTrailCSV(fileLocation string) (err error) {
+func parseTrailsCSV(fileLocation string) (err error) {
 	csvFile, err := os.Open(fileLocation)
 	if err != nil {
 		fmt.Printf("couldn't open csv at location %v", fileLocation)
@@ -69,33 +71,66 @@ func parseTrailCSV(fileLocation string) (err error) {
 		return err
 	}
 
+	bikeTrails = filterBikeTrails()
+
 	return nil
 }
 
-func getAllTrails(c *gin.Context) {
-	c.JSON(http.StatusOK, trails)
-}
-
-func getBikeTrails(c *gin.Context) {
+func filterBikeTrails() ([]Trail) {
 	bikeTrails := []Trail{}
-
 	for _, trail := range trails {
 		if trail.IsBikeTrail.bool {
 			bikeTrails = append(bikeTrails, trail)
 		}
 	}
 
+	return bikeTrails
+}
+
+func getAllTrails(c *gin.Context) {
+	c.JSON(http.StatusOK, trails)
+}
+
+
+func getBikeTrails(c *gin.Context) {
 	c.JSON(http.StatusOK, bikeTrails)
 }
 
+func getTrailsWithGrills(c *gin.Context) {
+	trailsWithGrills := []Trail{}
+
+	for _, trail := range trails {
+		if trail.HasGrills.bool {
+			trailsWithGrills = append(trailsWithGrills, trail)
+		}
+	}
+
+	c.JSON(http.StatusOK, trailsWithGrills)
+}
+
+func getBikeTrailsPicnic(c *gin.Context) {
+	bikeTrailsWithPicnic := []Trail{}
+
+	for _, bikeTrail := range bikeTrails {
+		if bikeTrail.HasPicnic.bool {
+			bikeTrailsWithPicnic = append(bikeTrailsWithPicnic, bikeTrail)
+		}
+	}
+
+	c.JSON(http.StatusOK, bikeTrailsWithPicnic)
+}
+
 func main() {
-	if err := parseTrailCSV("BoulderTrailHeads.csv"); err!= nil {
+	if err := parseTrailsCSV("BoulderTrailHeads.csv"); err!= nil {
 		panic(err)
 	}
 
 	router := gin.Default()
 	router.GET("/trails", getAllTrails)
+	router.GET("/trails/with-grills", getTrailsWithGrills)
 	router.GET("/bike-trails/", getBikeTrails)
+	router.GET("/bike-trails/with-picinc", getBikeTrailsPicnic)
+	
 	
 	router.Run("localhost:8080")
 }
